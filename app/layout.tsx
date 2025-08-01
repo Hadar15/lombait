3f3ce419cbf02e2c45d5fc5b182d0e6988009604
Hadar15 <haidar.r2005@gmail.com>
2025-08-01 08:08:55 +0700

    Push

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,6 @@ export default function RootLayout({
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
-          media="print"
-          onLoad="this.media='all'"
         />
         <noscript>
           <link
