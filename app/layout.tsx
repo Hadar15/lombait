dbead6e05b1cd35477955379fe261cdf2d8dbeae
Hadar15 <haidar.r2005@gmail.com>
2025-08-01 08:05:36 +0700

    f

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,24 +24,21 @@ export default function RootLayout({
   return (
     <html lang="id">
       <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      <link
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
-      rel="stylesheet"
-      media="print"
-      onLoad={(e) => {
-      (e.target as HTMLLinkElement).media = 'all';
-      }}
-    />
-    <noscript>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
-      rel="stylesheet"
-    />
-    </noscript>
-    </head>
-
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+          media="print"
+          onLoad="this.media='all'"
+        />
+        <noscript>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </noscript>
+      </head>
       <body className="bg-black text-white antialiased overflow-x-hidden font-inter">
         <ScrollToTop />
         <Navbar />
